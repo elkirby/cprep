dee32b32ab9d8edc51b0ef9c577a9445e709ee10
erin ✌️ <kirbomatik@gmail.com>
2018-07-13 22:00:11 -0400

    Moved main up a directory, added a random value generator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
+int * generate_values() {
+	static int values[100];
+	for (int i=0; i < 100; ++i) {
+		values[i] = rand() % 100 + 1;
+	}
+
+	return values;
+}
 
 int main(int argc, const char * argv[]) {
     
-    std::cout <<  std::to_string(argc)+ "\n";
+    int *a = generate_values();
+    for (int i = 0; i < 100; ++i) {
+		std::cout << a[i] << " ";
+	}
     std::string string = "";
     for (int i = 1; i < argc; ++i) {
         string.append(argv[i]);
